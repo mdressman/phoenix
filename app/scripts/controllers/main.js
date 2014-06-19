ca373e36601df8db2e68894f906928957bdf03f6
Matthew Dressman <md@mdressman.com>
2014-06-19 16:08:19 -0700

    add more comments and polish

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,10 +4,12 @@ var app = angular.module('phoenixApp');
 
 app.controller('MainCtrl', function ($scope, $http) {
 
+  // get data and add to scope
   $http.get('/api/user_stats').success(function(userStats) {
     $scope.userStats = userStats;
   });
 
+  // display chart for each user's event values
   $scope.showChart = function(user) {
     $.plot(
       $('#flot-'+user.id), [{
@@ -27,6 +29,7 @@ app.controller('MainCtrl', function ($scope, $http) {
 
 });
 
+// include partial template
 app.directive('userCards', function() {
   return {
     templateUrl: 'partials/user-card'
