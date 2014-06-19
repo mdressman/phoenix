1f9d55fd8bbcaea859a742c8b13236df120bf42a
Matthew Dressman <md@mdressman.com>
2014-06-19 09:26:59 -0700

    display chart for each user, parse events_by_day data, use directive for each user_card

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,26 @@
 'use strict';
 
-angular.module('phoenixApp')
-  .controller('MainCtrl', function ($scope, $http) {
+var app = angular.module('phoenixApp');
 
-    $http.get('/api/user_stats').success(function(userStats) {
-      $scope.userStats = userStats;
-    });
+app.controller('MainCtrl', function ($scope, $http) {
 
+  $http.get('/api/user_stats').success(function(userStats) {
+    $scope.userStats = userStats;
   });
+
+  $scope.showChart = function(user) {
+    console.log(user.events_by_day);
+    $.plot(
+      $('#flot-'+user.id),
+      [ user.events_by_day ],
+      { xaxis: { min: 1, max: 30 } }
+    );
+  }
+
+});
+
+app.directive('userCards', function() {
+  return {
+    templateUrl: 'partials/user-card'
+  };
+});
